72067426e734adc9659ed0c85358a2389e9dc972
Edwin Laptop <edwin16x@gmail.com>
2026-04-05 12:29:39 -0600

    Add swarm protocol contracts

---
+++ Swarm.Protocol/TractorState.cs
@@ -0,0 +1,10 @@
+﻿namespace Swarm.Protocol;
+
+public class TractorState
+{
+    public Guid Id { get; set; } // License Plate
+    public int X {get; set;} // Current X in Grid
+    public int Y { get; set;} // Current Y in Grid
+    public string Status {get; set;} 
+    public float BatteryLevel {get; set;} 
+}
